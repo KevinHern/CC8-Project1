f6423d246020f54258184a53d900e961d2f5e71a
KevinHern <35639443+KevinHern@users.noreply.github.com>
2020-09-05 20:40:58 -0600

    Update v3.0

--- client.py
+++ client.py
@@ -1,115 +1,72 @@
-import socket
+from socket import *
+import os
 import random
 import log
+import tcp
 
-bufferSize = 1520
-window_size = 0x0001
-RTT = 0.150
+src_port = "KH"
+
+def make_file_words(filename):
+    body_words = []
+    if filename is not None:
+        batch_size = 4  # Read in chunks of 32 bit words
+        with open(filename, "rb") as file:
+            while True:
+                piece = file.read(batch_size)
+                if piece == b'':
+                    break
+
+                body_words += [int(piece.hex(), 16)]
+
+            file.close()
+
+            '''
+            file_size = os.path.getsize(filename)
+            bytes_read = 0
+            batch_size = 1460  # 1460 Bytes
+            while bytes_read < file_size:
+                bytes_to_read = batch_size if (bytes_read + batch_size) < file_size else file_size - bytes_read
+                piece = file.read(bytes_to_read)
+                print(piece)
+
+                bytes_read += bytes_to_read
+
+            '''
+    return body_words
 
 
-def client_send(logger, udpsocket, address, message):
+
+
+
+def handshake(logger, udpsocket, address):
+    # Send SYN
+    logger.log_this("Estableciendo conexion...")
+    seq = random.randint(0, 100)
+    header = tcp.make_tcp_header_words(src_port, "--", seq, 0x002, [])
+    print(header)
+    segment = tcp.encode_segment(header, [])
+
+    # Send and get ACK
+    header_response, body_response = [], []
     while True:
-        udpsocket.sendto(message, address)
-        try:
-            data, server = udpsocket.recvfrom(1024)
-        except socket.timeout:
-            logger.log_this("Request Timed Out, resending...")
+        logger.log_this("Enviando SYN. SEQ = " + str(seq))
+        header_response, body_response = tcp.send(logger, udpsocket, address, 0, segment, 0)
+        if (header_response[5] & (tcp.ACK | tcp.SYN)) > 0:
+            logger.log_this("ACK Received. Destination port identified: " + header_response[1])
+            break
+
+    # Send ACK
+    seq += 1
+    header = tcp.make_tcp_header_words(src_port, header_response[1], seq, 0x002, [])
+    segment = tcp.encode_segment(header, [])
+
+    logger.log_this("Enviando ACK. SEQ =" + str(seq))
+    udpsocket.sendto(str.encode(segment), address)
+    print(header_response[1])
+    return header_response[1]
 
 
-def do_word(word32, data, bits_to_move):
-    return word32 | (data << bits_to_move)
-
-def make_tcp_header_words(dst_port, seq_init, offset, flags, options):
-    # SRC and DST Ports
-    ports = [ord(c) for c in "KH"] + [ord(c) for c in dst_port]
-
-    # SEQ and ACK Init
-    sequence_number = seq_init
-    acknowledgment_number = sequence_number + 1
-
-    # Data Offset
-    data_offset = offset    # AKA, header's length. Must be multiple of number of 32 bit words
-
-    # Control Flags
-    ctrl_flags = b'000000000000' | flags
-
-    # Window Size
-    window = window_size
-
-    # Checksum
-    checksum = 0
-
-    # Urgent Pointer
-    urgent_pointer = 0
-
-    # Options
-    options = options
-
-    # Padding
-    padding = 0     # Must calculate it here
-
-    # -----------------------------------------------------------------
-
-    # Generating hex encoded header
-    header_words = [0x00000000 for i in range(5)]
-
-    # Ports
-    for i in range(4):
-        header_words[0] = do_word(header_words[0], ports[i] & 0x0000FFFF, 8*(3-i))
-
-    # Sequence Number
-    header_words[1] = do_word(header_words[1], sequence_number & 0xFFFFFFFF, 0)
-
-    # Ack Number
-    header_words[2] = do_word(header_words[2], acknowledgment_number & 0xFFFFFFFF, 0)
-
-    # Data offset, flags and window size
-    header_words[3] = do_word(header_words[3], data_offset & 0x0000000F, 28)
-    header_words[3] = do_word(header_words[3], ctrl_flags & 0x00000FFF, 16)
-    header_words[3] = do_word(header_words[3], window_size & 0x0000FFFF, 0)
-
-    # Checksum and Urgent Pointer
-    header_words[4] = do_word(header_words[4], checksum & 0x0000FFFF, 16)
-    header_words[4] = do_word(header_words[4], urgent_pointer & 0x0000FFFF, 0)
-
-    # Options
-    totalOptionsLength = 0
-    allOptionValues = []
-    for option in options:
-        totalOptionsLength += option[1]
-        if option[1] == 0:
-            allOptionValues += [0x00000000]
-        elif option[1] == 1:
-            allOptionValues += [0x00000001]
-        elif option[1] == 2:
-            allOptionValues += [0x00000002, 0x00000004, (option[2] & 0x0000FF00) >> 8, (option[2] & 0x000000FF)]
-        elif option[1] == 3:
-            allOptionValues += [0x00000003, 0x00000003, (option[2] & 0x000000FF)]
-
-    curIndex = 4
-    for i in range(len(allOptionValues)):
-        if (i % 4) == 0:
-            header_words += [0x00000000]
-            curIndex +=1
-        header_words[curIndex] = do_word(header_words[curIndex], allOptionValues[i], 8*(3-i))
-
-    # Returns in HEX all the words used to construct the header
-    return header_words
-
-
-def make_tcp_body_words(filename):
-    pass
-
-
-def encode_segment(header_words, body_words):
-    hex_stream = ""
-    all_words = header_words + body_words
-    for word in all_words:
-        hex_stream += format(word, 'x').upper()
-    return hex_stream
-
-
-def handshake():
+def end(seq):
     pass
 
 
@@ -117,17 +74,66 @@ def client_run(logger):
     logger = logger
 
     # Host (URL/IP)
-    host = input("Provide Host:\t")
+    #host = input("Provide Host:\t")
+    host = "127.0.0.1"
     # Port Number
-    port = input("Provide a valid Port Number:\t")
-    if port < 0 or port > 65535: port = input("Provide a valid Port Number:\t")
-    address = (host, 12000)
+    #port = int(input("Provide a valid Port Number:\t"))
+    port = 8080
+    while (port < 0) or (port > 65535): port = int(input("Provide a valid Port Number:\t"))
+    address = (host, port)
 
     # ---------------------------
 
     # Socket Creation
-    udpsocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    udpsocket.settimeout(RTT)  # Setting RTT to 150 Milliseconds
+    udpsocket = socket(family=AF_INET, type=SOCK_DGRAM)
+    udpsocket.settimeout(tcp.RTT)  # Setting RTT to 150 Milliseconds
     logger.log_this("Client is ready")
 
-    # ---------------------------
+    # --------------------------- HANDSHAKE
+
+    handshake(logger, udpsocket, address)
+
+    # --------------------------- SEND FILE EXTENSION
+
+
+
+    # --------------------------- READ FILE
+
+    '''
+    file_words = []
+    while True:
+        try:
+            filename = input("Provide the file name:\t")
+            file_words = make_file_words(filename)
+            logger.log_this("Reading and processing <" + filename + ">")
+            break
+        except:
+            logger.log_this("File not found <" + filename + ">")
+
+    tcp_body_segments = []
+    while len(file_words) > 0:
+        words_to_extract = 1460 if len(file_words) >= 1460 else len(file_words)
+        temp_list = []
+        for i in range(words_to_extract):
+            temp_list += [file_words.pop(0)]
+        tcp_body_segments += [temp_list]
+
+    # --------------------------- SEND FILE
+
+    sequence = 100
+    options = []
+    segment_number = 1
+    for segment_body_words in tcp_body_segments:
+        segment_header_words = make_tcp_header_words("DT", sequence, len(options) + 5, 0x00000000, options)
+        segment = encode_segment(segment_header_words, segment_body_words)
+        # Send here
+        client_send(logger, udpsocket, address, sequence + 1, segment, segment_number)
+        sequence += + 1
+        segment_number += 1
+
+    # --------------------------- END CONNECTION
+    
+    '''
+
+    end_seq = random.randint(500,1000)
+    end(end_seq)
