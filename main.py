77fc1536125385c6e9f2a071860a27402ad95485
KevinHern <35639443+KevinHern@users.noreply.github.com>
2020-09-05 20:50:35 -0600

    Update v3.0

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 
 # Imports
 import log
+import client
+import server
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -12,16 +14,19 @@ if __name__ == '__main__':
         mode = input("What operation mode do you want to run? Client(c)/Server(s)\t")
         if mode == "c":
             # Configure Logger
-            logger = log.Logger(0, 'client.app')
+            logger = log.Logger(0, 'client.log')
 
             # Start Client
+            client.client_run(logger)
+
             pass
         elif mode == "s":
             # Configure Logger
-            logger = log.Logger(1, 'server.app')
+            logger = log.Logger(1, 'server.log')
             logger.log_this("Server is ready")
 
             # Start Server
+            server.server_run(logger)
             pass
         else:
             print("Sorry, I didn't understands. Try again")
