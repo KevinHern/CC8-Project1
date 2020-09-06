e8db8b3ea025b393c344159d7c773bd1b1633d01
KevinHern <35639443+KevinHern@users.noreply.github.com>
2020-09-05 20:41:29 -0600

    Update v3.0

--- server.py
+++ server.py
@@ -1,40 +1,71 @@
+from socket import *
+import os
+import random
+import tcp
+import log
 
-import socket
+src_port = "DT"
 
- 
-localIP = "127.0.0.1"
-localPort   = 20001
-bufferSize  = 1024
+def handshake(logger, udpsocket):
+    # Getting SYN
+    address, dst_port = [], []
+    while True:
+        logger.log_this("Accepting connection...")
+        response = udpsocket.recvfrom(tcp.socket_buffer_size)
+        address = response[1]
+        logger.log_this("Received segment from address: <" + str(address[0]) + ", " + str(address[1]) + ">")
+        header_response, body_response = tcp.process_segment(logger, response[0], 0)
+        if header_response is None:
+            continue
+        for word in header_response:
+            print("Type: " + str(type(word)) + "\tContent: " + str(word))
+        if (header_response[5] & tcp.SYN) > 0:
+            logger.log_this("SYN received. Starting Handshake protocol...")
+            break
+        else:
+            logger.log_this("Rejected connection. SYN not found")
 
-msgFromServer   = "Hello UDP Client"
-bytesToSend = str.encode(msgFromServer)
+    # Sending SYN + ACK
+    seq = 1
+    header = tcp.make_tcp_header_words(src_port, header_response[1], seq, tcp.ACK | tcp.SYN, [])
+    segment = tcp.encode_segment(header, [])
+    tcp.send(logger, udpsocket, address, seq + 1, segment, 1)
 
-# Create a datagram socket
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    return address, header_response[1]
+    pass
 
-# Bind to address and ip
-UDPServerSocket.bind((localIP, localPort))
 
-print("UDP server up and listening")
+def end():
+    pass
 
-# Listen for incoming datagrams
-while(True):
 
-	bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+def server_run(logger):
+    logger = logger
 
-	message = bytesAddressPair[0]
+    # Host (URL/IP)
+    host = "127.0.0.1"
+    # Port Number
+    #port = int(input("Provide a valid Port Number:\t"))
+    port = 8080
+    while (port < 0) or (port > 65535): port = int(input("Provide a valid Port Number:\t"))
+    address = (host, 12000)
 
-	address = bytesAddressPair[1]
+    # ---------------------------
 
-	clientMsg = "Message from Client:{}".format(message)
-	clientIP  = "Client IP Address:{}".format(address)
+    # Socket Creation
+    logger.log_this("Server is ready. Listening...")
+    udpsocket = socket(family=AF_INET, type=SOCK_DGRAM)
+    udpsocket.bind((host, port))
 
-	print(clientMsg)
-	print(clientIP)
 
-   
+    # --------------------------- HANDSHAKE
 
-	# Sending a reply to client
+    client_address, client_port = handshake(logger, udpsocket)
 
-	UDPServerSocket.sendto(bytesToSend, address)
-	#UDPServerSocket.close()
+    # --------------------------- GET FILE EXTENSION
+
+    # --------------------------- SAVING FILE
+
+    # --------------------------- END CONNECTION
+
+    end()
